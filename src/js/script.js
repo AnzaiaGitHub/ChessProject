99d395b5d5ac362ce3269b743eabaeb25cc1a6a8
Julian Pedroza <julianpedrozagarcia@gmail.com>
2024-05-19 00:47:43 -0500

    add some methods and logics to restrict the pawn movements

--- src/js/script.js
+++ src/js/script.js
@@ -67,26 +67,17 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function getPosibleMovementSquares(fromSquare){
-    let eatingSquares = [];
-    let movementSquares = [];
-    const pieceIndex = Object.keys(pieces).find((e) => {
-      return pieces[e] == fromSquare.textContent
-    });
-    const isWhite = pieceIndex === pieceIndex.toUpperCase();
-    switch (pieceIndex) {
-      case 'p': //black pawn
-      case 'P': //white pawn
-        const eatingCoordinates = [
-          [getTowards(isWhite,1),getLeft(isWhite,1)],
-          [getTowards(isWhite,1),getLeft(isWhite,-1)]
-        ];
-        eatingSquares = getCalculatedMovementSquares(fromSquare,eatingCoordinates);
-        const movementCoordinates = [
-          [getTowards(isWhite,1),0]
-        ];
-        movementSquares = getCalculatedMovementSquares(fromSquare,movementCoordinates);
-        break;
-    }
+    const pieceIndex = getPieceIndexFromFigure(fromSquare.textContent);
+    const isWhite = isWhitePiece(fromSquare);
+
+    const eatingCoordinates = getEatingCoordinates(fromSquare);
+    const eatingSquares = getCalculatedMovementSquares(fromSquare,eatingCoordinates);
+    validateEatingSquares(isWhite, eatingSquares);
+
+    const movementCoordinates = getMovementCoordinates(fromSquare);
+    const movementSquares = getCalculatedMovementSquares(fromSquare,movementCoordinates);
+    validateMovementSquares(isWhite,movementSquares)
+
     return eatingSquares.concat(movementSquares);
   }
 
@@ -131,6 +122,74 @@ document.addEventListener('DOMContentLoaded', () => {
 
   function getLeft(isWhite,numberOfSquares) {
     return getTowards(isWhite,numberOfSquares);
+  }
+
+  function validateEatingSquares(isWhite,eatingSquares){
+    const clearFromIndex = [];
+    eatingSquares.map((square, index) => {
+      if(
+        square.textContent == "" ||
+        isWhitePiece(square) !== !isWhite
+      ){
+        clearFromIndex.unshift(index);
+      }
+    });
+    clearFromIndex.map((index)=>{
+      eatingSquares.splice(index,1);
+    });
+  }
+
+  function validateMovementSquares(isWhite,movementSquares){
+    const clearFromIndex = [];
+    movementSquares.map((square, index) => {
+      if(
+        square.textContent != ""
+      ){
+        clearFromIndex.unshift(index);
+      }
+    });
+    clearFromIndex.map((index)=>{
+      movementSquares.splice(index,1);
+    });
+  }
+
+  function isWhitePiece(square){
+    const pieceIndex = getPieceIndexFromFigure(square.textContent);
+    return pieceIndex == pieceIndex.toUpperCase();
+  }
+
+  function getPieceIndexFromFigure(figure){
+    return Object.keys(pieces).find((e) => {
+      return pieces[e] == figure
+    });
+  }
+
+  function getEatingCoordinates(fromSquare){
+    const pieceIndex = getPieceIndexFromFigure(fromSquare.textContent);
+    const isWhite = isWhitePiece(fromSquare);
+    switch (pieceIndex.toUpperCase()) {
+      case 'P': //pawn
+        //TODO passant capture
+        return [
+          [getTowards(isWhite,1),getLeft(isWhite,1)],
+          [getTowards(isWhite,1),getLeft(isWhite,-1)]
+        ];
+        break;
+      default:
+        return getMovementCoordinates(fromSquare)
+    }
+  }
+
+  function getMovementCoordinates(fromSquare){
+    const pieceIndex =getPieceIndexFromFigure(fromSquare.textContent);
+    const isWhite = isWhitePiece(fromSquare);
+    switch (pieceIndex.toUpperCase()) {
+      case 'P':
+        return [
+          [getTowards(isWhite,1),0]
+        ];
+        break;
+    }
   }
 
   createBoard();
